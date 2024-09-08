1ec15bb56043a2af460809b24d8bbfe69ba363c1
OrzMiku <miku@ecy.pink>
2024-09-08 17:26:45 +0800

    矩形绘制测试和中文注释

--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,40 @@ int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
+    // 初始化 GLFW 库
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    // 创建一个窗口对象，其参数依次为：宽、高、窗口标题、显示模式、共享窗口
+    window = glfwCreateWindow(500, 500, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
+    // 将窗口的 OpenGL 上下文设置为当前上下文
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    // 循环渲染
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        // 设置清空颜色
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // 清空颜色缓冲
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        // 绘制一个矩形
+        glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
+
+        // 交换缓冲区
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // 检查是否有事件触发
         glfwPollEvents();
     }
 
+    // 释放窗口资源
     glfwTerminate();
     return 0;
 }
