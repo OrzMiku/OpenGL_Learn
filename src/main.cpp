38ae2e167dfa286df9bbdbf582dbe47a34990675
OrzMiku <miku@ecy.pink>
2024-11-13 14:41:12 +0800

    Separate the shader class implementation from the HPP file and refactor the comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,35 @@
+/**
+ * @file main.cpp
+ * @brief OpenGL learning project main file - renders multiple rotating 3D cubes with texture
+ */
+
 #define STB_IMAGE_IMPLEMENTATION
+#define GLM_ENABLE_EXPERIMENTAL
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
 #include <stb/stb_image.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/string_cast.hpp>
 #include "shader.hpp"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
-// 窗口大小
 const unsigned int SCR_WIDTH = 600;
 const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-    // GLFW 初始化和配置
+    // GLFW Init
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                 // 主版本号
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                 // 次版本号
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 核心模式
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // 创建窗口对象
+    // Create Window
     GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn_OpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -33,25 +37,25 @@ int main()
         glfwTerminate();
         return -1;
     }
-    // 设置窗口图标
     GLFWimage images[1];
     images[0].pixels = stbi_load("assets/textures/block.png", &images[0].width, &images[0].height, 0, 4);
     glfwSetWindowIcon(window, 1, images);
 
+    // Make Context Current
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // GLAD 初始化
+    // GLAD Init
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // 创建着色器程序
+    // Shader
     Shader ourShader("assets/shaders/main.vs", "assets/shaders/main.fs");
 
-    // 顶点数据
+    // Vertex Data
     float vertices[] = {
         -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
         0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
@@ -95,6 +99,7 @@ int main()
         -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
         -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
 
+    // Cube Positions
     glm::vec3 cubePositions[] = {
         glm::vec3(0.0f, 0.0f, 0.0f),
         glm::vec3(2.0f, 5.0f, -15.0f),
@@ -107,67 +112,64 @@ int main()
         glm::vec3(1.5f, 0.2f, -1.5f),
         glm::vec3(-1.3f, 1.0f, -1.5f)};
 
-    /**
-     * VBO 用来存储顶点数据
-     * VAO 用来存储顶点属性配置
-     * EBO 用来存储索引数据
-     */
-    unsigned int VBO, VAO;                                                     // 顶点缓冲对象 ID 顶点数组对象 ID
-    glGenVertexArrays(1, &VAO);                                                // 创建顶点数组对象
-    glGenBuffers(1, &VBO);                                                     // 创建缓冲对象
-    glBindVertexArray(VAO);                                                    // 绑定顶点数组对象
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);                                        // 绑定缓冲对象
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // 将顶点数据复制到缓冲中
+    // Setup VBO
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Attr 设置
+    // Setup VAO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    /**
-     * 已经将数据存入缓冲对象，不需要对 VBO 进行操作，可以解绑缓冲对象
-     * 已经将 VAO 配置好了，不需要对 VAO 进行操作，可以解绑顶点数组对象
-     */
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // 解绑缓冲对象，保证其他地方不会不小心使用它
-    glBindVertexArray(0);             // 解绑顶点数组对象，保证其他地方不会不小心使用它
+    // Unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
-    // 材质
+    // Texture
     unsigned int texture;
     glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture); // 绑定纹理
-    // 设置纹理环绕方式
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // x 轴
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // y 轴
-    // 设置纹理过滤方式
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // 缩小
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);              // 放大
-    // 加载纹理
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // Set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // Set the texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    // Load and generate the texture
     int width, height, nrChannels;
     unsigned char *data = stbi_load("assets/textures/block.png", &width, &height, &nrChannels, 0);
     if (data)
     {
-        // 生成纹理
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data); // 生成纹理
-        glGenerateMipmap(GL_TEXTURE_2D);                                                            // 生成多级渐远纹理
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
     {
         std::cout << "Failed to load texture" << std::endl;
     }
+    // Free image
+    stbi_image_free(data);
 
-    // 渲染循环
     while (!glfwWindowShouldClose(window))
     {
-        // 输入
+        // Handle Input
         processInput(window);
 
-        // 渲染
+        // Rendering
         glEnable(GL_DEPTH_TEST);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        ourShader.use(); // 使用着色器程序
+        ourShader.use();
 
         glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
@@ -190,25 +192,33 @@ int main()
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
-        // 检查并调用事件，交换缓冲
+        // Swap Buffers and Poll IO Events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // 释放资源
+    // De-allocate resources
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
 }
 
-// 窗口大小改变时调用
+/**
+ * @brief Callback function for window resize event
+ * @param window Pointer to the GLFW window
+ * @param width New width of the window
+ * @param height New height of the window
+ */
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
-// 处理输入
+/**
+ * @brief Process keyboard input
+ * @param window Pointer to the GLFW window
+ */
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
