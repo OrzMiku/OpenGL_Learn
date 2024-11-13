2ae2c44f932d91a932da41225439c966d82d0931
OrzMiku <miku@ecy.pink>
2024-11-13 10:37:31 +0800

    Add glm

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "shader.hpp"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
